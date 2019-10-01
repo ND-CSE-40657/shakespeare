ddca4444f75729e0160cc4486856ac0285ee2c37
Nicholas Marcopoli <nick@marcopo.li>
2019-10-01 13:37:10 -0400

    remove redundant import

--- fst.py
+++ fst.py
@@ -1,6 +1,5 @@
 import collections
 import collections.abc
-from collections import defaultdict
 import math
 
 EPSILON="ε" # Special symbol that is treated as the empty string
@@ -31,7 +30,7 @@ class Transition(collections.namedtuple('Transition', 'q a b r')):
 
 def _lambda_defaultdict_set():
     """Lift lambda: defaultdict(set) to global scope to enable pickling."""
-    return defaultdict(set)
+    return collections.defaultdict(set)
     
 class FST(object):
     """A finite state transducer."""
